2e5f586f423999e05a042f195ba646426ed11b13
Victor Manuel Martinez Cruz <vicmarc30@gmail.com>
2025-06-11 20:05:37 -0600

    feat: move map initialization to separate file and include in Bento section

--- src/animation/map.js
+++ src/animation/map.js
@@ -1,11 +1,12 @@
 import L from 'leaflet'
 
-// Espera a que el DOM esté listo
 setTimeout(() => {
   const map = L.map('map').setView([19.0413, -98.2062], 13) // Puebla
+
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors',
   }).addTo(map)
+
   L.marker([19.0413, -98.2062])
     .addTo(map)
     .bindPopup('GUDAS Studio - Puebla')
